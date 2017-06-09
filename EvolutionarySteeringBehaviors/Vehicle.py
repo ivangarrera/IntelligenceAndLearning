ede4c485814f95a6d59e424f67d60f5b3b65db20
ivangarrera <iesivangarcia@gmail.com>
2017-06-09 14:20:09 +0200

    Vehicles change its color depending on its health. More food and poison. DNA weight added.

--- EvolutionarySteeringBehaviors/Vehicle.py
+++ EvolutionarySteeringBehaviors/Vehicle.py
@@ -1,4 +1,6 @@
 from math import sqrt
+import random
+import pygame
 import math
 
 
@@ -8,18 +10,24 @@ class Vehicle:
         self.position = [x, y]
         self.max_speed = 5.5
         self.max_force = 0.9
+        self.health = 1
+        self.dna = []
+        self.dna = self.create_dna(self.dna)
 
-    def apply_force(self, force):
+    def apply_force(self, force, dna_ind):
+        if dna_ind != -1:
+            for i in range(len(force)):
+                force[i] *= self.dna[dna_ind]
         steering_force = self.truncate(force, self.max_force)
         self.velocity = self.truncate(self.add(self.velocity, steering_force), self.max_speed)
         self.position = self.add(self.position, self.velocity)
 
     # This method calculates the steering force where:
     # steer = desired - velocity
-    def seek(self, target):
+    def seek(self, target, dna_ind):
         desired_velocity = self.mult(self.normalize(self.sub(target, self.position)), self.max_speed)
         steering_force = self.sub(desired_velocity, self.velocity)
-        self.apply_force(steering_force)
+        self.apply_force(steering_force, dna_ind)
 
     def magnitude(self, vector):
         return sqrt(sum(vector[i]*vector[i] for i in range(len(vector))))
@@ -49,18 +57,72 @@ class Vehicle:
     def absolute(self, vector):
         return [abs(vector[i]) for i in range(len(vector))]
 
-    def eat(self, food):
-        closest = 100000  # infinity
-        closest_index = -1
-        for i in range(len(food)):
-            x_food_location = food[i][0]
-            y_food_location = food[i][1]
-            distance = sqrt(math.pow(x_food_location-self.position[0], 2) +
-                            math.pow(y_food_location-self.position[1], 2))
-            if distance < closest:
-                closest = distance
-                closest_index = i
+    def which_is_closest(self, vect):
+        if len(vect) > 0:
+            closest = 100000  # infinity
+            closest_index = -1
+            for i in range(len(vect)):
+                x_food_location = vect[i][0]
+                y_food_location = vect[i][1]
+                distance = sqrt(math.pow(x_food_location-self.position[0], 2) +
+                                math.pow(y_food_location-self.position[1], 2))
 
-        return closest_index
+                if distance < closest:
+                    closest = distance
+                    closest_index = i
+        return closest, closest_index
 
+    def eat(self, vector, index, distance, nutrition, dna_ind):
+        element = vector[index]
+        if distance < 5:
+            if nutrition > 0:
+                self.increase_health(nutrition)
+            else:
+                self.decrease_health(nutrition*-1)
+            vector.remove(element)
 
+        elif index > -1:
+            self.seek(vector[index], dna_ind)
+        return vector, element
+
+    def draw_vehicle(self, screen, color, rect):
+        pygame.draw.rect(screen, color, rect)
+
+    def decrease_health(self, value):
+        self.health -= value
+
+    def increase_health(self, value):
+        if self.health + value > 1:
+            self.health = 1
+        else:
+            self.health += value
+
+    def create_dna(self, dna):
+        dna.append(random.uniform(-2, 2))  # Food weight
+        dna.append(random.uniform(-2, 2))  # Poison weight
+        dna.append(random.uniform(10, 100))  # Food perception
+        dna.append(random.uniform(10, 100))  # Poison perception
+        return dna
+
+    # To don't permit vehicle to 'leave' the area (the screen)
+    def boundaries(self, width, height):
+        dist = 10
+        desired = None
+        if self.position[0] < dist:
+            desired = [self.max_speed, self.velocity[1]]
+        elif self.position[0] < width - dist:
+            desired = [-self.max_speed, self.velocity[1]]
+
+        if self.position[1] < dist:
+            desired = [self.velocity[0], self.max_speed]
+        elif self.position[1] > height - dist:
+            desired = [self.velocity[0], -self.max_speed]
+
+        if desired is not None:
+            desired = self.normalize(desired)
+            steer = self.sub(desired, self.velocity)
+            steer = self.truncate(steer, self.max_force)
+            self.apply_force(steer, -1)
+
+    """def behaviors(self, good, bad):
+        steer_force_good = self.eat(vector, index, distance, nutrition)"""
--- EvolutionarySteeringBehaviors/gui.py
+++ EvolutionarySteeringBehaviors/gui.py
@@ -1,9 +1,10 @@
 import pygame, sys
 import random
-from pygame.locals import *
 import Vehicle
+from colour import Color
 
-pygame.mixer.init()
+# CONSTANTS
+
 #             R    G    B
 WHITE     = (255, 255, 255)
 BLACK     = (  0,   0,   0)
@@ -13,58 +14,136 @@ DARKGREEN = (  0, 155,   0)
 DARKGRAY  = ( 40,  40,  40)
 BGCOLOR   = BLACK
 
-pygame.init()
-
-WIDTH = 500
-HEIGHT = 400
-NUM_FOOD = 30
-food = []
-
+WIDTH           = 700
+HEIGHT          = 700
+NUM_FOOD        = 200
+NUM_POISON      = 20
+NUM_VEHICLES    = 5
+food            = []
+poison          = []
+figures         = []
+poison_figures  = []
+vehicles = []
+vehicles_figures = []
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-vehicle_figure = pygame.Rect(0, 0, 15, 15)
-vehicle = Vehicle.Vehicle(100, 100)
 
-figures = []
+green = Color("green")
+colors = list(green.range_to(Color("red"), 10))
 
-for i in range(NUM_FOOD):
-    x = random.uniform(0, WIDTH)
-    y = random.uniform(0, HEIGHT)
-    food.append([x, y])
+class Gui:
+    def __init__(self, food, poison, figures, poison_figures, screen, vehicles, vehicles_figures):
+        pygame.mixer.init()
+        pygame.init()
 
+        # Create food rectangle objects
+        for i in range(NUM_FOOD):
+            x = random.uniform(0, WIDTH)
+            y = random.uniform(0, HEIGHT)
+            food.append([x, y])
 
-def draw_eat():
-    for j in range(len(food)):
-        figures.append(pygame.Rect(food[j][0], food[j][1], 7, 7))
-        pygame.draw.rect(screen, DARKGREEN, figures[j])
+        # Create poison rectangle objects
+        for i in range(NUM_POISON):
+            x = random.uniform(0, WIDTH)
+            y = random.uniform(0, HEIGHT)
+            poison.append([x, y])
 
-clock = pygame.time.Clock()
-draw_eat()
-pygame.display.update()
-while True:
-    for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-    screen.fill(BLACK)
-    pygame.draw.rect(screen, BGCOLOR, vehicle_figure)
+        # Create vehicle rectangle objects
+        for i in range(NUM_VEHICLES):
+            x = random.uniform(0, WIDTH)
+            y = random.uniform(0, HEIGHT)
+            vehicles.append(Vehicle.Vehicle(x, y))
 
-    # Calculate the new vehicle's position
-    index = vehicle.eat(food)
-    vehicle.seek(food[index])
-    x_v, y_v = vehicle.position
+        clock = pygame.time.Clock()
+        self.draw_vehicles()
 
-    # Print the food
-    for i in range(NUM_FOOD):
-        rec = figures[i]
-        pygame.draw.rect(screen, DARKGREEN, rec)
+        while True:
+            for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                            pygame.quit()
+                            sys.exit()
+            screen.fill(BLACK)
 
-    # Draw the vehicle in the new position
-    vehicle_figure.x = x_v
-    vehicle_figure.y = y_v
-    vehicle_figure.move_ip(x_v - food[index][0], y_v - food[index][1])
-    pygame.draw.rect(screen, WHITE, vehicle_figure)
+            # Renovate food, approximately each 25% of time
+            if random.uniform(0, 1) > 0.75:
+                x = random.uniform(0, WIDTH)
+                y = random.uniform(0, HEIGHT)
+                food.append([x, y])
 
-    pygame.display.update()
+            # Renovate poison, approximately each 1% of time
+            if random.uniform(0, 1) > 0.95:
+                x = random.uniform(0, WIDTH)
+                y = random.uniform(0, HEIGHT)
+                poison.append([x, y])
 
-    clock.tick(60)
+            # Perform the action for all vehicles
+            if len(vehicles) > 0:
+                for i in range(len(vehicles)):
+                    if vehicles[i].health > 0:
+                        # Decrease health periodically
+                        vehicles[i].decrease_health(0.005)
 
+                        # Calculate the nearest target (food or poison)
+                        best_food_distance, index = vehicles[i].which_is_closest(food)
+                        best_poison_distance, ind = vehicles[i].which_is_closest(poison)
+                        if best_food_distance < best_poison_distance:
+                            vehicles[i].boundaries(WIDTH, HEIGHT)
+                            food, element = vehicles[i].eat(food, index, best_food_distance, 0.1, 0)
+                        else:
+                            vehicles[i].boundaries(WIDTH, HEIGHT)
+                            poison, element = vehicles[i].eat(poison, ind, best_poison_distance, -0.3, 1)
+
+                        # Calculate the new vehicle's position
+                        vehicles[i].seek(element, -1)
+                        x_v, y_v = vehicles[i].position
+
+                        # Draw the vehicle in the new position
+                        vehicles_figures[i].x = x_v
+                        vehicles_figures[i].y = y_v
+                        vehicles_figures[i].move_ip(x_v - element[0], y_v - element[1])
+                    else:  # Not health enough. Vehicle broken.
+                        vehicles.remove(vehicles[i])
+                        break
+            else:
+                print("All vehicles are broken")
+                exit(0)
+
+            # Clear list, because can causes overwrites.
+            figures.clear()
+            vehicles_figures.clear()
+            poison_figures.clear()
+
+            # Draw the eat
+            self.draw_eat()
+
+            # Draw the poison
+            self.draw_poison()
+
+            self.draw_vehicles()
+
+            pygame.display.update()
+
+            clock.tick(60)
+
+    # Methods
+
+    def draw_eat(self):
+        for j in range(len(food)):
+            figures.append(pygame.Rect(food[j][0], food[j][1], 7, 7))
+            pygame.draw.rect(screen, DARKGREEN, figures[j])
+
+    def draw_poison(self):
+        for i in range(len(poison)):
+            poison_figures.append(pygame.Rect(poison[i][0], poison[i][1], 7, 7))
+            pygame.draw.rect(screen, RED, poison_figures[i])
+
+    def draw_vehicles(self):
+        for i in range(len(vehicles)):
+            vehicles_figures.append(pygame.Rect(vehicles[i].position[0], vehicles[i].position[1], 15, 15))
+            color = colors[int(vehicles[i].health * 10 - 1)]
+            _red = 255 - color.get_rgb()[0]*255
+            _green = 255 - color.get_rgb()[1]*255
+            _blue = color.get_rgb()[2]*255
+            color = (_red, _green, _blue)
+            pygame.draw.rect(screen, color, vehicles_figures[i])
+
+gui = Gui(food, poison, figures, poison_figures, screen, vehicles, vehicles_figures)
